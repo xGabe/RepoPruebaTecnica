c438397529a83fafd7f292fab20d496bb085c6ce
Gabriel Paganelli <gabriel.paganelli@evoltis.com>
2024-06-29 01:11:10 +0200

    validate room price test

--- cypress/e2e/validateRoomPrice.cy.js
+++ cypress/e2e/validateRoomPrice.cy.js
@@ -9,16 +9,18 @@ describe("Check room types",()=>{
         })
         cy.daySelect().then((randomDates)=>{
             const [date1,date2] = randomDates
-            cy.calculateDays(date1,date2).then((Days)=>{
-                const cost1 = Days*20  //single room
-                const cost2 = Days*30  //double room
-                const cost3 = Days*40  //triple room 
-                const cost4 = Days*50  //quadruple room
-                // assert value for the stay
-                cy.get("tbody tr").eq(0).should("contain.text", cost1) //single room
-                cy.get("tbody tr").eq(1).should("contain.text", cost2) //double room
-                cy.get("tbody tr").eq(2).should("contain.text", cost3) //triple room
-                cy.get("tbody tr").eq(3).should("contain.text", cost4) //quadruple room
+            cy.fixture("priceRooms").then((the)=>{
+                cy.calculateDays(date1,date2).then((Days)=>{
+                    const cost1 = Days*the.simple  //single room
+                    const cost2 = Days*the.double  //double room
+                    const cost3 = Days*the.triple  //triple room 
+                    const cost4 = Days*the.quadruple  //quadruple room
+                    // assert value for the stay
+                    cy.get("tbody tr").eq(0).should("contain.text", cost1) //single room
+                    cy.get("tbody tr").eq(1).should("contain.text", cost2) //double room
+                    cy.get("tbody tr").eq(2).should("contain.text", cost3) //triple room
+                    cy.get("tbody tr").eq(3).should("contain.text", cost4) //quadruple room
+                })
             })
         })
 
