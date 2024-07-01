53ffd35493dfd9efbb6a095f754d4251d31281b8
Gabriel Paganelli <gabriel.paganelli@evoltis.com>
2024-07-02 01:09:09 +0200

    fix test

--- cypress/e2e/roomTypes/validateRoomPrice.cy.js
+++ cypress/e2e/roomTypes/validateRoomPrice.cy.js
@@ -10,26 +10,21 @@ describe("Validate price for room and ",()=>{
         cy.selectDateAvailable().then(({ startDate, endDate, priceRoomType, rdmIndex })=>{
             cy.fixture("priceRooms").then((the)=>{
                 cy.calculateDays(startDate,endDate).then((Days)=>{
-                    const cost1 = Days*the.simple  //single room
-                    const cost2 = Days*the.double  //double room
-                    const cost3 = Days*the.triple  //triple room 
-                    const cost4 = Days*the.quadruple  //quadruple room
+                    const roomTypes = ['simple', 'double', 'triple', 'quadruple'];
+                    const roomCosts = roomTypes.map(roomType => Days * the[roomType]);
                     // assert value for the stay
-                    cy.get("tbody tr").eq(0).should("contain.text", cost1) //single room
-                    cy.get("tbody tr").eq(1).should("contain.text", cost2) //double room
-                    cy.get("tbody tr").eq(2).should("contain.text", cost3) //triple room
-                    cy.get("tbody tr").eq(3).should("contain.text", cost4) //quadruple room
+                    roomCosts.forEach((cost, index) => {
+                        cy.get("tbody tr").eq(index).should("contain.text", cost);
+                      });
                     cy.fixture("roomsType").then((the)=>{
                         cy.get(the.nextStep).eq(rdmIndex).click()
                     })
                     //assert value in booking form
-                    // cy.selectRoomType().then((priceRoomType)=>{
                         cy.fixture("createBookingPage").then((the)=>{
                             cy.get(the.inputText).eq(2).invoke('val').then((price)=>{
                                 expect(price).deep.equal(priceRoomType)
                             })
                         })
-                    // })
                 })
             })
         })
